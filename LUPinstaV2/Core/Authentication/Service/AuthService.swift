06a76d6cb9756347c31f3e9148fef1ec9f01910c
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-03-25 18:30:45 +0530

    progress/ role based signup WORKS

--- LUPinstaV2/Core/Authentication/Service/AuthService.swift
+++ LUPinstaV2/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestoreSwift
+import Firebase
 
 class AuthService{
     @Published var userSession:FirebaseAuth.User?
@@ -34,6 +36,12 @@ class AuthService{
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            
+            await uploadUserData(uid: result.user.uid,
+                                 username: username,
+                                 email: email,
+                                 role: role)
+            
         }catch {
             print("DEBUG: Failed to create user with error:\(error.localizedDescription)")
         }
@@ -47,5 +55,24 @@ class AuthService{
     func signout(){
         try? Auth.auth().signOut()
         self.userSession = nil
+        
     }
+    
+    private func uploadUserData(uid:String, username:String, email:String, role:String) async {
+        let user = User(id: uid,
+                        email: email,
+                        role: role,
+                        username: username)
+        
+        // encode the user before sending it to FB
+        //guard let cause it might fail
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else {return}
+        
+        //set the data finally
+        try? await Firestore.firestore()
+            .collection("users") //which collection or table
+            .document(user.id)  //what document?
+            .setData(encodedUser)  //set this
+    }
+    
 }
--- LUPinstaV2/Model/User.swift
+++ LUPinstaV2/Model/User.swift
@@ -10,6 +10,7 @@ import Foundation
 struct User: Identifiable,Hashable,Codable {
     let id: String
     let email: String
+    let role: String //important
     
     var username: String
     var profileImageUrl: String?
@@ -21,35 +22,35 @@ struct User: Identifiable,Hashable,Codable {
 extension User {
     static var MOCK_USERS:[User] = [
         .init(id: NSUUID().uuidString,
-              email: "jotoro@example.com",
+              email: "jotoro@example.com", role: "Trainee",
               username: "jotaro_kujo",
               profileImageUrl: "jotaro",
               fullname: "jotoro kujo",
               bio: "ora ora"),
         
             .init(id: NSUUID().uuidString,
-                  email: "dio@example.com",
+                  email: "dio@example.com", role: "Trainee",
                   username: "kono_dio_da",
                   profileImageUrl: "dio",
                   fullname: "dio ",
                   bio: "muda muda muda"),
         
             .init(id: NSUUID().uuidString,
-                  email: "pucci@example.com",
+                  email: "pucci@example.com", role: "Coach",
                   username: "xim_sama",
                   profileImageUrl: "pucci",
                   fullname: "pucci sama",
                   bio: "I love the full moon"),
         
             .init(id: NSUUID().uuidString,
-                  email: "kira@example.com",
+                  email: "kira@example.com", role: "Coach",
                   username: "kira_yoshkaghe",
                   profileImageUrl: "kira",
                   fullname: "kira yoshkaghe",
                   bio: "watshino no nawa kirsa yoshkage"),
         
             .init(id: NSUUID().uuidString,
-                  email: "giono@example.com",
+                  email: "giono@example.com", role: "Coach",
                   username: "giorono_giovanna",
                   profileImageUrl: "giorno",
                   fullname: "giorno giovanna",
