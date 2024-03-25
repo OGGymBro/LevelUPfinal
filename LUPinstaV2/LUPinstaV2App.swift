8b2ed25275528254a6cdc20794fa387f596c3c79
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-03-25 09:26:14 +0530

    Initial Commit

---
+++ LUPinstaV2/LUPinstaV2App.swift
@@ -0,0 +1,17 @@
+//
+//  LUPinstaV2App.swift
+//  LUPinstaV2
+//
+//  Created by Joaquim Menezes on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LUPinstaV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
