745a76576fb48715cdf48d3b06092d49a529b928
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-03-27 10:02:37 +0530

    STAGE 22 done ,profile image uploding done

--- LUPinstaV2/Core/Profile/ViewModel/EditProfileViewModel.swift
+++ LUPinstaV2/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -23,6 +23,8 @@ class EditProfileViewModel:ObservableObject {
     @Published  var fullname = ""
     @Published  var bio = ""
     
+    private var uiImage:UIImage?
+    
     init(user:User){  //init user when file is called
         self.user = user
     }
@@ -32,16 +34,23 @@ class EditProfileViewModel:ObservableObject {
         
         guard let data = try? await item.loadTransferable(type: Data.self) else {return}
         guard let uiImage = UIImage(data: data) else {return}
+        self.uiImage = uiImage
         self.profileImage = Image(uiImage:  uiImage)
         
     }
     
     func updateUserData() async throws {
-        //update profile if changed
-        
-        
-        
         var data = [String:Any]()
+        
+        
+        //update profile if changed
+        if let uiImage = uiImage {
+            let imageUrl = try? await ImageUploader.uploadImage(image: uiImage)
+            data["profileImageUrl"] = imageUrl
+        }
+        
+        
+        
         //update name if changed
         if !fullname.isEmpty && user.fullname != fullname {
             data["fullname"] = fullname
