c38fe28438ca0ff645760fd43f5e5365496ace8e
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-03-28 21:15:11 +0530

    Role based navigation ACHIEVED !!!

---
+++ LUPinstaV2/Coach App/CoachTabBar/CoachTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CoachTabView.swift
+//  LUPinstaV2
+//
+//  Created by Joaquim Menezes on 28/03/24.
+//
+
+import SwiftUI
+
+struct CoachTabView: View {
+    var body: some View {
+        Text("Hello, Coach!")
+    }
+}
+
+#Preview {
+    CoachTabView()
+}
--- LUPinstaV2/Core/Root/View/ContentView.swift
+++ LUPinstaV2/Core/Root/View/ContentView.swift
@@ -11,16 +11,56 @@ struct ContentView: View {
     @StateObject var registrationViewModel = RegistrationViewModel()
     
     var body: some View {
-        Group {
-            if viewModel.userSession == nil {
-                LoginView()
-                    .environmentObject(registrationViewModel)
-            }else if let currentUser = viewModel.currentUser {
-                MainTabView(user: currentUser)
+            Group {
+                if viewModel.userSession == nil {
+                    LoginView()
+                        .environmentObject(registrationViewModel)
+                } else if let currentUser = viewModel.currentUser {
+                    // Check the role directly without using == for comparison
+                    if currentUser.role == "Coach" {
+                        CoachTabView()
+                    } else if currentUser.role == "Trainee" {
+                        MainTabView(user: currentUser)
+                    } else {
+                        // Handle unexpected roles here
+                        Text("Unknown role")
+                    }
+                }
             }
         }
-    }
 }
 #Preview {
     ContentView()
 }
+
+//Group {
+//    if viewModel.userSession == nil {
+//        // If user session is nil, show the LoginView
+//        LoginView()
+//            .environmentObject(registrationViewModel)
+//    } else if let currentUser = viewModel.currentUser {
+//        // If user is logged in, check their role
+//        switch currentUser.role {
+//        case "Trainee":
+//            // If role is Trainee, navigate to MainTabView
+//            MainTabView()
+//        case "Coach":
+//            // If role is Coach, navigate to CoachTabView
+//            CoachTabView()
+//        default:
+//            // Handle unexpected roles here
+//            Text("Unknown role")
+//        }
+//    }
+//}
+
+
+
+
+//if viewModel.userSession == nil {
+//    LoginView()
+//        .environmentObject(registrationViewModel)
+//}else if let currentUser = viewModel.currentUser {
+//    MainTabView(user: currentUser)
+//}
+
