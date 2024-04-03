81377222f74ce29dc6af9c0f5e2e3930f37c7268
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-04-03 23:04:33 +0530

    BUG FIX: current user excluded in search

--- LUPinstaV2/Services/UserService.swift
+++ LUPinstaV2/Services/UserService.swift
@@ -29,9 +29,14 @@ struct UserService{
     }
     
     static func fetchAllTrainers() async throws -> [User] {
+        guard let currentUserUID = Auth.auth().currentUser?.uid else {
+               // Throw an error or return an empty array if current user UID is nil
+            return []
+           }
         let snapshot = try await Firestore.firestore()
             .collection("users")
             .whereField("role", isEqualTo: "Coach")
+            //.whereField("uid", isNotEqualTo: currentUserUID) // Exclude current user
             .getDocuments()
         
         return snapshot.documents.compactMap({try? $0.data(as: User.self)})
